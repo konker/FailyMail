d847af94715d94f7db0a6fbc3f70eac469e8b0e6
konker <konker@morningwoodsoftware.com>
2010-02-24 00:41:43 +0200

    Some amends for non-iphone clients

--- html/static/scripts/main.js
+++ html/static/scripts/main.js
@@ -5,14 +5,17 @@
 var main = (function() {
     return {
         _curPage: 1,
+        _isIPhone: false,
 
         init: function() {
+            main._isIPhone = (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i));
+
             $('.refresh').bind('click', function() {
-                if (main._curPage == 1) {
-                    main._curPage = 2;
+                if (main._curPage == 2 || !main._isIPhone) {
+                    main._curPage = 1;
                 }
                 else {
-                    main._curPage = 1;
+                    main._curPage = 2;
                 }
 
                 main.clearHeadline();
@@ -36,20 +39,24 @@ var main = (function() {
             $('#wrapper' + main._curPage + ' .headline').html(headlines.util.breakWords(headlines.getHeadline(), 9));
         },
         renderCopy: function() {
-            $('#wrapper' + main._curPage + ' .copy .col1').html(drivel.drivel({ maxwords: 40, maxwordlen: 12, dwords: ['dirvel'], 'break2': ' '}));
-            $('#wrapper' + main._curPage + ' .copy .col2').html(drivel.drivel({ maxwords: 40, maxwordlen: 12, dwords: ['dirvel'], 'break2': ' ', strongStart: '"', strongEnd: '"'}));
-            $('#wrapper' + main._curPage + ' .copy .col3').html(drivel.drivel({ maxwords: 40, maxwordlen: 12, dwords: ['dirvel'], 'break2': ' ', strongStart: '"', strongEnd: '"'}));
+            $('#wrapper' + main._curPage + ' .copy .col1').html(drivel.drivel({ maxwords: 40, maxwordlen: 11, dwords: ['dirvel'], 'break2': ' '}));
+            $('#wrapper' + main._curPage + ' .copy .col2').html(drivel.drivel({ maxwords: 40, maxwordlen: 11, dwords: ['dirvel'], 'break2': ' ', strongStart: '"', strongEnd: '"'}));
+            $('#wrapper' + main._curPage + ' .copy .col3').html(drivel.drivel({ maxwords: 40, maxwordlen: 11, dwords: ['dirvel'], 'break2': ' ', strongStart: '"', strongEnd: '"'}));
         }
     }
 })();
-$.jQTouch({
-    icon: 'jqtouch.png',
-    statusBar: 'black-translucent',
-    preloadImages: [
-        'static/jqtouch/themes/konker/img/chevron_white.png',
-        'static/jqtouch/themes/konker/img/bg_row_select.gif',
-        'static/jqtouch/themes/konker/img/back_button_clicked.png',
-        'static/jqtouch/themes/konker/img/button_clicked.png'
-        ]
-});
+
+// don't let non-iphone clients try the fancy stuff
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) {
+    $.jQTouch({
+        icon: 'jqtouch.png',
+        statusBar: 'black',
+        preloadImages: [
+            'static/jqtouch/themes/konker/img/chevron_white.png',
+            'static/jqtouch/themes/konker/img/bg_row_select.gif',
+            'static/jqtouch/themes/konker/img/back_button_clicked.png',
+            'static/jqtouch/themes/konker/img/button_clicked.png'
+            ]
+    });
+}
 $(main.init);
