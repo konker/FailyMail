905c8b3541e0d6cb813fcb6d4ecc989bb07e6898
konker <konker@morningwoodsoftware.com>
2010-02-26 14:27:40 +0200

    Removed jQtouch javascript functionality. Changed transitions to jquery fade in/out. Added iphone meta/links to markup.

--- html/static/scripts/main.js
+++ html/static/scripts/main.js
@@ -11,15 +11,19 @@ var main = (function() {
             main._isIPhone = (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i));
 
             $('.refresh').bind('click', function() {
+                /*
                 if (main._curPage == 2 || !main._isIPhone) {
                     main._curPage = 1;
                 }
                 else {
                     main._curPage = 2;
                 }
+                */
 
+                /*
                 main.clearHeadline();
                 main.clearCopy();
+                */
 
                 main.renderHeadline();
                 main.renderCopy();
@@ -36,16 +40,22 @@ var main = (function() {
             $('#wrapper' + main._curPage + ' .copy .col').html('');
         },
         renderHeadline: function() {
-            $('#wrapper' + main._curPage + ' .headline').html(headlines.util.breakWords(headlines.getHeadline(), 9));
+            $('#wrapper' + main._curPage + ' .headline').fadeOut('fast', function() {
+                $('#wrapper' + main._curPage + ' .headline').html(headlines.util.breakWords(headlines.getHeadline(), 9)).fadeIn();
+            });
         },
         renderCopy: function() {
-            $('#wrapper' + main._curPage + ' .copy .col1').html(drivel.drivel({ maxwords: 40, maxwordlen: 11, dwords: ['dirvel'], 'break2': ' '}));
-            $('#wrapper' + main._curPage + ' .copy .col2').html(drivel.drivel({ maxwords: 40, maxwordlen: 11, dwords: ['dirvel'], 'break2': ' ', strongStart: '"', strongEnd: '"'}));
-            $('#wrapper' + main._curPage + ' .copy .col3').html(drivel.drivel({ maxwords: 40, maxwordlen: 11, dwords: ['dirvel'], 'break2': ' ', strongStart: '"', strongEnd: '"'}));
+            $('#wrapper' + main._curPage + ' .copy').fadeOut('fast', function() {
+                $('#wrapper' + main._curPage + ' .copy .col1').html(drivel.drivel({ maxwords: 40, maxwordlen: 11, dwords: ['dirvel'], 'break2': ' '}));
+                $('#wrapper' + main._curPage + ' .copy .col2').html(drivel.drivel({ maxwords: 40, maxwordlen: 11, dwords: ['dirvel'], 'break2': ' ', strongStart: '"', strongEnd: '"'}));
+                $('#wrapper' + main._curPage + ' .copy .col3').html(drivel.drivel({ maxwords: 40, maxwordlen: 11, dwords: ['dirvel'], 'break2': ' ', strongStart: '"', strongEnd: '"'}));
+                $('#wrapper' + main._curPage + ' .copy').fadeIn();
+            });
         }
     }
 })();
 
+/*
 // don't let non-iphone clients try the fancy stuff
 if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) {
     $.jQTouch({
@@ -59,4 +69,5 @@ if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) 
             ]
     });
 }
+*/
 $(main.init);
